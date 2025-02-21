6d3721f9728ac5024158889ceb473ba6f923aa78
cristian <cristiandres1317@gmail.com>
2025-02-21 11:11:35 -0500

    funcion para agregar amigo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // mi primer hola mundo 
 console.log('hola, mundo');
 // array donde almacenará los nombres de los amigos ingresados.
-let Amigos = [];
+let amigos = [];
 
 //función donde permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos 
 function agregarAmigo (){
@@ -16,15 +16,17 @@ function agregarAmigo (){
     }
 
     //validar que el nombre no es de duplicado
-    if(Amigos.includes(nombreAmigo)){
+    if(amigos.includes(nombreAmigo)){
         alert(`el nombre ${nombreAmigo} se encuentra en la lista de amigos`);
     }
     //agregar el nombre al array de amigos
-    Amigos.push(nombreAmigo);
+    amigos.push(nombreAmigo);
 
     //limpiar el campo de entrada
     imputAmigo.ariaValu = '';
 
-    //
+    //Actualizar la lista en el html
+    acutualizarLista();
+
 }
 
