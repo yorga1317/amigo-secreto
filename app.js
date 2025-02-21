f078dd3ce4cd2ffb5e669704beb007b7ca41ef92
cristian <cristiandres1317@gmail.com>
2025-02-21 13:47:29 -0500

    juego de amigo secrrto

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById ("resultado");
 
-// array donde almacenará los nombres de los amigos ingresados.
-let amigos = [];
-
-//función donde permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos 
-function agregarAmigo (){
-    const verAmigo = document.getElementById ('Amigo');
-    const nombreAmigo = inputAmigo.ariaValueMax.trim();
-
-    //validar que el campo no este vacio
-    if (nombre = ''){
-        alert('Por favor, inserte un nombre');
-        return;
+function agregarAmigo(){
+    if(inputAmigo.value == ''){
+        alert ('debes ingresar un nombre.');
     }
+    // para empujar los nombre del imput a la lista de amigos con listaAmigos.push
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};   
 
-    //validar que el nombre no es de duplicado
-    if(amigos.includes(nombreAmigo)){
-        alert(`el nombre ${nombreAmigo} se encuentra en la lista de amigos`);
-    }
-    //agregar el nombre al array de amigos
-    amigos.push(nombreAmigo);
-
-    //limpiar el campo de entrada
-    imputAmigo.ariaValu = '';
-
-    //Actualizar la lista en el html
-    acutualizarLista();
-
-}
-// funcion para actualizar la lista de amigos en la interfaz.
-function acutualizarLista(){
-    const listaAmigos = document.getElementById('listaAmigos');
-// limpiar el contenido actual de la lista
-    listaAmigos.innerHTML = '';
- // recorrer el array en un ciclo for 
-    for(let i = 0; 1 <amigos.length; i++){
-        const li = document.createElement('li');
-        li.textContent = amigos(i);
-        listaAmigos.appendChild(li);
-
-    }
-}
-
-
-
-
+function sortearAmigo(){
+    const random = Math.floor((Math.random() * listaAmigos.length));
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es ${amigoSecreto}</li>`
+};
