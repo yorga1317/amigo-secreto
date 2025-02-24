c4a61d5a5757e1058b9f6f7679da9038a2ee5cde
cristian <cristiandres1317@gmail.com>
2025-02-24 10:27:07 -0500

    acutilizacion del juego

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ function agregarAmigo(){
     if(inputAmigo.value == ''){
         alert ('debes ingresar un nombre.');
     }
+    
+    if(listaAmigos.includes(inputAmigo.value)){
+        alert(`el nombre ${inputAmigo.value} ya esta en la lista`);
+        listaAmigos = listaAmigos.filter(amigo => amigo !== inputAmigo.value);
+    }
     // para empujar los nombre del imput a la lista de amigos con listaAmigos.push
     listaAmigos.push(inputAmigo.value);
     ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+
+limpiarCaja();
 };   
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
 
 function sortearAmigo(){
     const random = Math.floor((Math.random() * listaAmigos.length));
